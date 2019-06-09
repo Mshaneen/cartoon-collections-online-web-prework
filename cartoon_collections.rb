f46a038fe463979e93f3927638827782c111c071
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-09 05:46:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |types|
-   types.include?(cheese_types)
+   types.include?(cheese_types.join(","))
   end
 end
